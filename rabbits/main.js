502b0a37a8179f0a86cac9e0d47973fb03e8a1db
Muni <kangmoonee@gmail.com>
2021-01-25 20:33:52 +0900

    scroll into view-rabbit

---
+++ rabbits/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn");
+const rabbit = document.querySelector("#rabbit");
+
+btn.addEventListener("click", () => {
+  rabbit.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
