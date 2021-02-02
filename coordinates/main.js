9c57505caff3b2f8a6a8b24957f8377e5be8a0af
Muni <kangmoonee@gmail.com>
2021-02-02 09:43:32 +0900

    Edited to translate coordinates-target

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,17 +3,33 @@ const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
-  console.log(`${x} ${y}`);
+// 윈도우가 전부 다 로드가 되면, 그 때 타겟의 크기를 읽어와서 도큐먼트에 마우스 무브라는 이벤트를 등록한다.
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  // x, y가 0으로 타겟 이미지가 준비되지 않은 경우가 크다. 콘솔로그로 확인해보기
+  console.log(targetRect);
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    console.log(`${x} ${y}`);
 
-  tag.innerHTML = `${x}px, ${y}px`;
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+
+    // target.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+    // tag.style.top = `${y}px`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfWidth
+    }px)`;
+    tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
